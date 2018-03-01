9491b7478baa85957ed25b2de40c1a8b59430d8a
Yabir <yabirgb@gmail.com>
2018-03-01 19:54:28 +0100

    Moved info getter

---
+++ main.py
@@ -0,0 +1,36 @@
+def get_info(filename):
+    """
+    Function that gets the input
+    Return:
+        params: A tuple with the input params
+        rides: A list with all the rides info
+    """
+    with open(filename) as f:
+        #get the first line of the input
+        params = list(map(int, f.readline().split()))
+
+        #list with all the travels
+        rides = []
+
+        
+        for line in f:
+
+            #Get values from ride line
+            ride = list(map(int, line.split()))
+
+            #4-uple with origin and destination in that order
+            coordenates = tuple(ride[:4])
+
+            #Earliest step to pick
+            earliest = ride[-2]
+
+            #Latest tick to arrive at destination
+            latest = ride[-1]
+
+            #List of all rides
+            rides.append((coordenates, earliest, latest))
+
+
+        return params, rides
+
+print(get_info('a_example.in'))
