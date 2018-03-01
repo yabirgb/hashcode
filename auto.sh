4f20abaa2985e137b2c8e1ad8e546086aea095aa
Yabir <yabirgb@gmail.com>
2018-03-01 21:48:57 +0100

    Auto script

---
+++ auto.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+testa="a_example"
+testb="b_should_be_easy"
+testc="c_no_hurry"
+testd="d_metropolis"
+teste="e_high_bonus"
+
+python3 main.py $testa
+python3 main.py $testb
+python3 main.py $testc
+python3 main.py $testd
+python3 main.py $teste
