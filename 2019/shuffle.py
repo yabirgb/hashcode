c8f5973fbd0c498d41e496238b196ed08f79467a
Ocete <ocetebt@hotmail.com>
2019-02-28 22:16:36 +0100

    fixed

--- 2019/shuffle.py
+++ 2019/shuffle.py
@@ -15,7 +15,7 @@ def order_crossover(gen1, gen2, rt=None, rb=None):
 
     """
     Generates two childre from the parents provided using
-    Order Cross 
+    Order Cross
     """
 
     # len(gen) = n
@@ -32,10 +32,10 @@ def order_crossover(gen1, gen2, rt=None, rb=None):
     rand_top = rt or randint(3, size-3)+2
     rand_botton = rb or randint(0, rand_top-1)
 
-    # set of 
+    # set of
     used1 = set()
     used2 = set()
-    
+
     for i in range(rand_botton, rand_top+1):
         sld1 = gen1.slides[i]
         sld2 = gen2.slides[i]
@@ -43,12 +43,12 @@ def order_crossover(gen1, gen2, rt=None, rb=None):
         child1.append(sld1)
         child2.append(sld2)
 
-        used1.add(sld1.pk)
-        used2.add(sld2.pk)
+        used1.union(set([x.pk for x in sld1.photos if x!= None]))
+        used2.union(set([x.pk for x in sld2.photos if x!= None]))
 
     lower_limit = (rand_top + 1) % size
     top_limit = rand_top
-    
+
     pos = lower_limit
 
     while pos != top_limit:
@@ -56,19 +56,19 @@ def order_crossover(gen1, gen2, rt=None, rb=None):
         print(pos, " ", size)
         if gen2.slides[pos].pk not in used1:
             child1.append(gen2.slides[pos])
-            used1.add(gen2.slides[pos].pk)
+            used1.union(set([x.pk for x in gen2.slides[pos].photos if x!= None]))
 
         if gen1.slides[pos].pk not in used2:
             child2.append(gen1.slides[pos])
-            used2.add(gen1.slides[pos].pk)
+            used2.union(set([x.pk for x in gen1.slides[pos].photos if x!= None]))
 
-        pos = (pos + 1) % size 
+        pos = (pos + 1) % size
 
     if gen2.slides[rand_top].pk not in used1:
         child2.append(gen1.slides[rand_top])
     if gen1.slides[rand_top].pk not in used2:
-        child1.append(gen2.slides[rand_top]) 
+        child1.append(gen2.slides[rand_top])
 
     print("hygjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj")
 
-    return Individual(child1), Individual(child2)    
+    return Individual(child1), Individual(child2)
