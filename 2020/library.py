cbae2db9b107d7867fddf79e52e6bfe8ef204b5d
David Cabezas Berrido <dxabezas@gmail.com>
2020-02-20 19:36:17 +0100

    library heuristic

--- 2020/library.py
+++ 2020/library.py
@@ -10,3 +10,17 @@ class Library:
     def __str__(self):
         return f'Library with id {self.id_} has {len(self.books)} books. {self.per_day} books per day. Registration of {self.registration}'
         
+
+    def heuristic(self, remaining, scores, scanned):
+
+        b=(remaining-self.registration)*self.per_day
+
+        own_scores = [scores[i]*scanned[i] for i in self.books]
+        own_scores.sort(reverse=True)
+
+        h = 0
+
+        for i in range(0, b):
+            h+=own_scores[i]
+
+        return h
